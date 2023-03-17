48e3ff5b216fde226c4542c2781a997653e7e4c4
moses edache <justmosesedache>
2023-03-17 12:40:06 -0700

    task-5

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 /**
- * main - Entry point
+ * main - assign a random number to the variable n each time it is executed
+ *and print the last digit of the number stored in the variable n
  *
  * Return: Always 0 (Success)
  */
