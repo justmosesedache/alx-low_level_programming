01f9964614b0d19575b337f9e9e95a15dcfa975b
moses edache <justmosesedache>
2023-03-17 00:58:27 -0700

    corrections

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,7 +10,8 @@ int main(void)
         long int c;
         long long int d;
         float f;
-        printf("Size of a char: %lu bytes(S)\n", (unsigned long)sizeof(a));        printf("Size of a int: %lu bytes(S)\n", (unsigned long)sizeof(b));
+        printf("Size of a char: %lu bytes(S)\n", (unsigned long)sizeof(a));        
+	printf("Size of a int: %lu bytes(S)\n", (unsigned long)sizeof(b));
         printf("Size of a long int: %lu bytes(S)\n", (unsigned long)sizeof(c));
 	printf("Size of a long long int: %lu bytes(S)\n", (unsigned long)sizeof(d));
 	printf("Size of a float: %lu bytes(S)\n", (unsigned long)sizeof(f));
