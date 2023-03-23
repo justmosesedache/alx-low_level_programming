d66ee2a1f7850ce81240af2bcbf1bf316a867db3
moses edache <justmosesedache>
2023-03-23 06:11:53 -0700

    first 5 mandatory tasks

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * 2-mul.c -a function that multiplies two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: 0
+ */
+
+
+int mul(int a, int b);
+{
+	return (a * b);
+}
---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_numbers(void)- a function that prints the numbers, from 0 to 9,
+ * Return: 0
+ *
+*/
+
+void print_numbers(void);
+{
+	char i;
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar("%d", i);
+	}
+	_putchar('\n')
+	return (0);
+}
