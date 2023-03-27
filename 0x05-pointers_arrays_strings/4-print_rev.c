0a71ce128b6c0707631708092a64ca036baec8ae
moses edache <justmosesedache>
2023-03-27 14:13:13 -0700

    task-5

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_rev - prints string in rev
+ * @s: string to be printed
+ */
+
+void print_rev(char *s)
+{
+	int a, i;
+
+	while (s[a] != '\0')
+		a++;
+	for (i = a - 1; a >= ; i--)
+	{
+		_putchar(s[a]);
+	}
+	_putchar('\n')
+}
