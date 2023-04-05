ec2a1707b8e733ac89449ac392c55edb8c711f91
moses edache <justmosesedache>
2023-04-05 05:39:10 -0700

    first task

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,4 +1,5 @@
-
+#ifndef MAIN_H
+#define MAIN_H
 
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
@@ -7,5 +8,5 @@ int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
-
-
+int _putchar(char c);
+#endif
